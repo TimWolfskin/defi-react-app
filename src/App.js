4384b138b8b616e66110e8c3f717d719379c0c88
Timur Aliev <timwolfskin013.gmail.com>
2022-03-15 10:22:20 +0300

    added developers component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import About from './components/about/About';
+import Developer from './components/developer/Developer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Developer />
     </div>
   );
 }
