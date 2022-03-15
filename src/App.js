5cfef022a6ef08018b2054bea157ac65158d5324
Timur Aliev <timwolfskin013.gmail.com>
2022-03-15 13:35:27 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/navbar/Navbar';
 import About from './components/about/About';
 import Developer from './components/developer/Developer';
 import Subscribe from './components/subscribe/Subscribe';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Developer />
       <Subscribe />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,50 @@
+import React from "react";
+import './Footer.css';
+
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="container">
+                <div className="col">
+                    <h4>Products</h4>
+                    <p>App</p>
+                    <p>Analytics</p>
+                    <p>Token Lists</p>
+                    <p>Defi</p>
+                </div>
+                <div className="col">
+                    <h4>Developers</h4>
+                    <p>App</p>
+                    <p>Analytics</p>
+                    <p>Token Lists</p>
+                    <p>Defi</p>
+                </div>
+                <div className="col">
+                    <h4>Governance</h4>
+                    <p>App</p>
+                    <p>Analytics</p>
+                    <p>Token Lists</p>
+                    <p>Defi</p>
+                </div>
+                <div className="col">
+                    <h4>Community</h4>
+                    <p>Reddit</p>
+                    <p>Twitter</p>
+                    <p>Discord</p>
+                    <p>Defi</p>
+                </div>
+                <div className="col">
+                    <h4>About</h4>
+                    <p>Blog</p>
+                    <p>info</p>
+                    <p>FAQ</p>
+                    <p>jOBS</p>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+
+export default Footer;
